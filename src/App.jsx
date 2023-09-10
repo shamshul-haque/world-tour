aa2a3c6a9d0d8c329cd8184467cc51ed44830d03
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-10 16:44:36 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>World Tour</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Countries = () => {
+  return (
+    <div>
+      <h1>country</h1>
+    </div>
+  );
+};
+
+export default Countries;
