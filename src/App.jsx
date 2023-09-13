11a72d6ac7b223b1d16777ab1a7cb3df9b3de7e8
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-13 23:25:10 +0600

    title center

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Countries from "./components/Countries";
 function App() {
   return (
     <>
-      <h1>My World Tour</h1>
+      <h1 className="text-center">My World Tour</h1>
       <Countries />
     </>
   );
